25be7a6f03d1408508ad8036a605726d77de87e5
byenzinnn <enzodearaujogrellmann@hotmail.com>
2024-06-06 12:58:48 -0300

    Itens adicionados

---
+++ src/components/navbar.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "../assets/css/nav.css";
+import imgLogo from "../assets/img/imgLogo-Deive.png"
+
+function Navbar() {
+  const [active, setActive] = useState("navMenu");
+  const [toggleIcon, setToggleIcon] = useState("navToggler");
+
+  const navToggle = () => {
+    active === "navMenu"
+      ? setActive("navMenu navActive")
+      : setActive("navMenu");
+
+  //Toggler icon
+    toggleIcon === "navToggler"
+      ? setToggleIcon("navToggler toggle")
+      : setToggleIcon("navToggler");
+  };
+
+  return (
+    <nav className="nav">
+      <img className="imgLogo" src={imgLogo} alt="Associação Evangelística"  />
+      <ul className={active}>
+        <li className="navItem">
+          <a href="#" className="navLink">
+            HOME
+          </a>
+        </li>
+        <li className="navItem">
+          <a href="#" className="navLink">
+            DOAÇÕES
+          </a>
+        </li>
+        <li className="navItem">
+          <a href="#" className="navLink">
+            TRANSPARÊNCIA
+          </a>
+        </li>
+        <li className="navItem">
+          <a href="#" className="navLink">
+            NOTÍCIAS
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={toggleIcon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
