aece37cab40dd1478ca1d809848f6ed7457d4588
byenzinnn <enzodearaujogrellmann@hotmail.com>
2024-07-10 16:18:19 -0300

    Items added & updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,9 +14,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.transform = "translateY(0px)";
   } else {
-    document.getElementById("navbar").style.top = "-900px";
+    document.getElementById("navbar").style.transform = "translateY(-300px)";
   }
   prevScrollpos = currentScrollPos;
 }
